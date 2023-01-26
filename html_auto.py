f23cbdf104b44c9a1d845fc618eca883e39196a2
xm01 <xm01@qq.com>
2023-01-26 13:37:39 +0800

    推荐

--- html_auto.py
+++ html_auto.py
@@ -130,7 +130,7 @@ class ReqTols(object):
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title></title>
+<title>推荐网址</title>
 <link rel="stylesheet" href="he_js_css_img/main.css">
 </head>
 <body>

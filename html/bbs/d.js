8f45912d381789b714fb36d51cecd54d568fd7cc
xm01 <xm01@qq.com>
2023-06-26 17:47:16 +0800

    1

--- html/bbs/d.js
+++ html/bbs/d.js
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-<title>404 - File or directory not found.</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312"/>
+<title>404 - �Ҳ����ļ���Ŀ¼��</title>
 <style type="text/css">
 <!--
 body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}
@@ -18,11 +18,11 @@ background-color:#555555;}
 </style>
 </head>
 <body>
-<div id="header"><h1>Server Error</h1></div>
+<div id="header"><h1>����������</h1></div>
 <div id="content">
  <div class="content-container"><fieldset>
-  <h2>404 - File or directory not found.</h2>
-  <h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>
+  <h2>404 - �Ҳ����ļ���Ŀ¼��</h2>
+  <h3>��Ҫ���ҵ���Դ�����ѱ�ɾ�����Ѹ������ƻ�����ʱ�����á�</h3>
  </fieldset></div>
 </div>
 </body>

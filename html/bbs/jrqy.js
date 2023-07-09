4080dc2eb5ed232de9cf6306823a125b970f6418
xm01 <xm01@qq.com>
2023-07-09 16:49:44 +0800

    1

--- html/bbs/jrqy.js
+++ html/bbs/jrqy.js
@@ -14,7 +14,7 @@ document.writeln(".bizhong1234 img {width:100%; border-collapse: collapse; font-
 document.writeln("</style>");
 document.writeln("<div class=\'bizhong1234 clearfix\'>");
 document.writeln("    <div class=\'bizhong1234-box action-bz\'>");
-document.writeln("<img src=\'/img/jrqy189.jpg\' id=\'jrqypicx\'>");
+document.writeln("<img src=\'/img/jrqy190.jpg\' id=\'jrqypicx\'>");
 document.writeln("    </div>");
 document.writeln("    ");
 document.writeln("</div>");
@@ -25,6 +25,8 @@ document.writeln(" <span class=\'bizhong1234-btn bizhong1234-next\' onclick=\'ne
 document.writeln("</div>");
 document.writeln("<script>");
 document.writeln("    var picArrsbx=new Array(");
+
+document.writeln("\'/img/jrqy190.jpg\',");
 document.writeln("\'/img/jrqy189.jpg\',");
 document.writeln("\'/img/jrqy188.jpg\',");
 document.writeln("\'/img/jrqy187.jpg\',");

c762f3404bf11bbb6ea3f96d0d7e8def45b1e293
xm01 <xm01@qq.com>
2023-07-12 17:53:02 +0800

    1

--- html/bbs/xgjrtg.js
+++ html/bbs/xgjrtg.js
@@ -10,30 +10,31 @@ document.writeln('<div class="unsites"><ul>');
 
 
 
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://61921a.com/#45612">077期：天选之人【10码中特】13中13&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.391199b.com/pt/268.html">077期：罗坦神威【平特一肖】19中18&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://192255.com/gsb/020.html ">077期：影月无痕 【绝杀二肖】14中14&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://272733b.com/gsb/084.html">077期：快乐人生【家野中特】10中10&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://325577.com/gsb/0009.htm ">077期：伴你終老【绝杀三肖】27中27&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://663385c.com/gsb/067.htm">077期：家住长安【琴棋书画】12中11&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://550118.com/gsb/060.html">077期：惊弓之鸟【大小中特】20中18&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://669925a.com/gsb3/jsyx.html ">077期：默默无闻【绝杀一肖】72中71&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://669971b.com/gsb1/g198.htm">077期：发财小哥【绝杀四码】57中56&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://496688c.com/shagsb/s01.html">077期：鬼斧神刀【绝杀半波】23中23&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://118sjkj.com/gsb/dxzt.html">077期：美丽邂逅【大小中特】18中16&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://49279c.com/gsb/318.html">077期：@李老师【七尾中特】19中17&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://31087.com/bbs1/251.htm ">077期：冲关成功【野兽家禽】19中18&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://118379.cc/zlyq/9477.html">077期：玉洁冰清【大小中特】13中12&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://01736.com/gsbb2/06.html">077期：虎背熊腰 【稳杀四肖】18中16&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.760077b.com/gsb/003.html">077期：红尘烟雨【七尾中特】13中12&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.290996a.com/xsq/234011.html">077期：雷锋论坛【绝杀二肖】54中51&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.890099.com/gsb/069.html ">077期：风光无限【家野中特】14中13&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.735858a.com/top/1700.html">077期：群雄逐鹿【四肖中特】10中09&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://593399a.com/gsb1/392.html">077期：启天大胜*单双中特 12中12&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://775593.com/gsb/028.htm ">077期：下山抓妖【来料六肖】14中13&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://628866a.com/bbs/0149.html ">077期：丰裕人物【大小中特】13中13&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://510066b.com/gsb/020.html">077期：暧心少年【精杀一头 】16中15&#10004;</a></b></li>');
-document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://905566b.com/bbs/37.html">077期：电动马达【绝杀一肖】65中63&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://61921a.com/#45612">078期：天选之人【10码中特】13中13&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.391199b.com/pt/268.html">078期：罗坦神威【平特一肖】22中20&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://192255.com/gsb/057.html">078期：世事茫茫【三头中特】15中15&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://272733b.com/gsb/084.html">078期：快乐人生【家野中特】12中11&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://325577.com/gsb/0009.htm ">078期：伴你終老【绝杀三肖】29中29&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://663385c.com/gsb/067.htm">078期：家住长安【琴棋书画】14中13&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://550118.com/gsb/060.html">078期：惊弓之鸟【大小中特】22中19&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://669925a.com/gsb3/jsyx.html ">078期：默默无闻【绝杀一肖】77中75&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://669971b.com/gsb1/g198.htm">078期：发财小哥【绝杀四码】59中57&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://496688c.com/pmgsb/p06.html">078期：@冰红茶【平特一肖】11中10&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://118sjkj.com/gsb/dxzt.html">078期：美丽邂逅【大小中特】20中18&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://49279c.com/gsb/318.html">078期：@李老师【七尾中特】21中19&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://31087.com/bbs1/251.htm ">078期：冲关成功【野兽家禽】21中20&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://118379.cc/zlyq/9477.html">078期：玉洁冰清【大小中特】14中12&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://01736.com/gsbb2/45.html">078期：@小王子【规律双波】15中14&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.760077b.com/gsb/133.html">078期：独钓风雪【绝杀一头】16中15&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.290996a.com/xsq/234011.html">078期：雷锋论坛【绝杀二肖】57中54&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.890099.com/gsb/069.html ">078期：风光无限【家野中特】16中14&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://www.735858a.com/top/1700.html">078期：群雄逐鹿【四肖中特】12中10&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://593399a.com/gsb1/392.html">078期：启天大胜【单双中特】14中14&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://775593.com/gsb/029.htm">078期：仙女下凡【七肖中特】14中14&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://628866a.com/bbs/0149.html ">078期：丰裕人物【大小中特】15中14&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://510066b.com/gsb/083.html">078期：拔树搜根【精准单双】12中12&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://905566b.com/bbs/37.html">078期：电动马达【绝杀一肖】65中63&#10004;</a></b></li>');
+document.writeln('<li style="background-color:#ffffff"><b><a target="_blank" href="https://40489a.com/gsb/051.html">078期：心存一人【④肖中特】10中9&#10004;</a></b></li>');
 
 
 document.writeln('<div class="clearfix"></div></ul></div>');

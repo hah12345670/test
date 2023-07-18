a2ce85a8dec58079b1932e10cfbdfe768fe5211d
xm01 <xm01@qq.com>
2023-07-18 17:15:58 +0800

    1

--- html/bbs/yy4wzt.js
+++ html/bbs/yy4wzt.js
@@ -20,7 +20,7 @@ document.writeln("");
 document.writeln("<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'41\'><tbody><tr>");
 document.writeln("	<td width=100% height=41><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
-document.writeln("198期<font color=\'#FF00FF\'>（兔龙蛇+0612尾）</font>开？00</font></b></font></table>");
+document.writeln("199期<font color=\'#FF00FF\'>（鼠狗兔+0129尾）</font>开？00</font></b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("");

c21319d76f447e8230e636b1d7b1c0e4b940ec27
xm01 <xm01@qq.com>
2023-10-19 11:11:23 +0800

    123

--- html/bbs/gjpjt.js
+++ html/bbs/gjpjt.js
@@ -23,10 +23,11 @@ document.writeln("");
 document.writeln("	<tr>");
 document.writeln("      <td align=\'left\' height=45 bgcolor=\'#FFFFFF\' ><p align=\'center\'>");
 document.writeln("		<font face=\'微软雅黑\'>");
-document.writeln("		<font color=\'#000000\' style=\'font-size: 12pt; font-weight:700\'>291期</font><font color=\'#FF0000\' style=\'font-size: 12pt; font-weight:700\'>(五米三步马在追)(亡羊补牢)</font><font style=\'font-size: 12pt; font-weight:700\'>开特00平00</font></font></td>");
+document.writeln("		<font color=\'#000000\' style=\'font-size: 12pt; font-weight:700\'>292期</font><font color=\'#FF0000\' style=\'font-size: 12pt; font-weight:700\'>(金木土火有数拣)(画蛇添足)</font><font style=\'font-size: 12pt; font-weight:700\'>开特00平00</font></font></td>");
 document.writeln("    </tr>    ");
 document.writeln("    <!--结束-->");
 document.writeln("");
+
 document.writeln("          <!--开始-->");
 document.writeln("");
 document.writeln("	<tr>");

a2102e61cc05960776061c66436a295b0619d7d4
xm01 <xm01@qq.com>
2024-01-04 00:18:40 +0800

    1

--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -20,12 +20,15 @@ document.writeln(" ");
 
 
 
-
+document.writeln("  <tr>");
+document.writeln("    <td>004期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【风雨电】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>003期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雷风电】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>003期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【<span style=\'background-color: #FFFF00\'>雷</span>风电】</font>开<font color=\'#ac6a00\'>鸡19</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 
--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -20,14 +20,14 @@ document.writeln("");
 
 
 
-
-
-
+document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>003期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>004期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
-document.writeln("");
+document.writeln("   ");
+
+
 
 
 document.writeln("");

6d808c51d9e62ad1c4fd3bfc6224b37a3e27124e
xm01 <xm01@qq.com>
2024-01-08 10:53:06 +0800

    1

--- html/he77/cxqd.js
+++ html/he77/cxqd.js
@@ -29,12 +29,16 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("   <tr>");
+document.writeln("    <td>008期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【秋春夏】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>007期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【冬秋春】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>007期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【冬<span style=\'background-color: #FFFF00\'>秋</span>春】</font>开<font color=\'#ac6a00\'>鸡19</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 
--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -22,8 +22,28 @@ document.writeln(" ");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>007期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雷雨风】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>008期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【风雨电】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
+
+
+document.writeln("  <tr>");
+document.writeln("    <td>007期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【<span style=\'background-color: #FFFF00\'>雷</span>雨风】</font>开<font color=\'#ac6a00\'>鸡19</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

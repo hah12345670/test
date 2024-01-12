e54b48f279df8bb05381b1834691c02ce9542bc2
xm01 <xm01@qq.com>
2024-01-13 01:00:19 +0800

    1

--- html/he77/qqsh.js
+++ html/he77/qqsh.js
@@ -28,7 +28,7 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>012期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书画琴】</font>开<font color=\'#ac6a00\'>⒛</font>准</td>");
+document.writeln("    <td>012期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书画琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
 
 

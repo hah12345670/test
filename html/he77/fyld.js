3a8c275be1822e043237afac84ab5c6354f42fc0
xm01 <xm01@qq.com>
2024-01-24 09:40:11 +0800

    1

--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -21,12 +21,16 @@ document.writeln(" ");
 
 
 
-
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>024期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨电雷】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>023期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨雷风】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>023期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨<span style=\'background-color: #FFFF00\'>雷</span>风】</font>开<font color=\'#ac6a00\'>猴20</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

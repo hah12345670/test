289d1781a1712d06762cd60c39e53366b13808a5
xm <xm@qq.com>
2024-02-07 07:17:30 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -27,14 +27,13 @@ document.writeln("");
 
 
 
-
-
-
+document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>037期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>038期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【4444头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
-document.writeln("   ");
+
+
 
 
 document.writeln("");

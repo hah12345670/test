6bbe25dbdb2ae2f1dcbcec82f57b4d30714e31f2
xm <xm@qq.com>
2024-02-12 10:20:34 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -22,15 +22,14 @@ document.writeln("");
 
 
 
-
-
-
-
+document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>042期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>043期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【4444头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
-document.writeln("   ");
+
+
+
 
 
 document.writeln("");

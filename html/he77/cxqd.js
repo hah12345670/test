27c6e152e7a9bc786a8c871a8a7f008a964c3557
xm01 <xm01@qq.com>
2024-02-16 00:07:35 +0800

    1

--- html/he77/cxqd.js
+++ html/he77/cxqd.js
@@ -23,12 +23,17 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>047期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【春夏冬】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("   <tr>");
-document.writeln("    <td>046期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【秋春夏】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>046期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【秋<span style=\'background-color: #FFFF00\'>春</span>夏】</font>开<font color=\'#ac6a00\'>兔02</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln(" ");

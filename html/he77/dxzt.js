aecd3d47d1fb45a1e7f8ae16d7039005148c19ef
xm01 <xm01@qq.com>
2024-02-17 01:57:45 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -20,12 +20,15 @@ document.writeln("");
 
 
 
-
+document.writeln("  <tr>");
+document.writeln("    <td>048期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>047期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>047期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>猴09</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");

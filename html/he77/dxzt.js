742a17305dd71ef78be37c77ad701abed691b2a3
xm01 <xm01@qq.com>
2024-03-03 00:39:30 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -16,8 +16,31 @@ document.writeln("");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.writeln("  <tr>");
-document.writeln("    <td>062期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>063期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+
+
+document.writeln("  <tr>");
+document.writeln("    <td>062期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>龙49</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 
--- html/he77/qqsh.js
+++ html/he77/qqsh.js
@@ -20,14 +20,14 @@ document.writeln("");
 
 
 
-
-
-
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>062期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【画琴棋】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>063期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书棋琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
+
+
+
 
 
 document.writeln("");

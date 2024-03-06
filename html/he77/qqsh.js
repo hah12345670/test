16ca6a78037dc1e3d4daa6c92be4af97daaabbd3
xm01 <xm01@qq.com>
2024-03-06 11:27:02 +0800

    1

--- html/he77/qqsh.js
+++ html/he77/qqsh.js
@@ -28,12 +28,16 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>066期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书画琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>065期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【画棋书】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>065期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【画<span style=\'background-color: #FFFF00\'>棋</span>书】</font>开<font color=\'#ac6a00\'>牛04</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

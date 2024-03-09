d1e12a028cf50bd07a68ca3e9468bc8bfa9daf8e
xm01 <xm01@qq.com>
2024-03-10 00:18:17 +0800

    1

--- html/he77/cxqd.js
+++ html/he77/cxqd.js
@@ -23,15 +23,14 @@ document.writeln("");
 
 
 
-
-
-
 document.writeln("");
-document.writeln("  <tr>");
-document.writeln("    <td>069期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【春夏冬】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("   <tr>");
+document.writeln("    <td>070期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【秋春夏】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
-document.writeln("");
+document.writeln(" ");
+
+
 
 
 document.writeln("");
--- html/he77/jsbb.js
+++ html/he77/jsbb.js
@@ -19,12 +19,17 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>070期<font color=\'#ac6a00\'>绝杀半波</font><font color=\'#0000FF\'>【绿波单】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>069期<font color=\'#ac6a00\'>绝杀半波</font><font color=\'#0000FF\'>【蓝波单】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>069期<font color=\'#ac6a00\'>绝杀半波</font><font color=\'#0000FF\'>【蓝波单】</font>开<font color=\'#ac6a00\'>鸡08</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

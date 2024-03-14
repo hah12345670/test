16d9d845e583b7ed20c082df785fb5ac9f57708e
xm01 <xm01@qq.com>
2024-03-15 00:51:10 +0800

    1

--- html/he77/jsyw.js
+++ html/he77/jsyw.js
@@ -32,14 +32,17 @@ document.writeln("");
 
 
 
-
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>075期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【1111尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>074期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【7777尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>074期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【7777尾】</font>开<font color=\'#ac6a00\'>牛16</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

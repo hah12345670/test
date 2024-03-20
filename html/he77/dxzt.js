c41a0fdb398e53bc00bada4f21faab2824ca8d91
xm01 <xm01@qq.com>
2024-03-21 01:27:10 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -24,15 +24,13 @@ document.writeln("");
 
 
 
-
-
-
-document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>080期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>081期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
-document.writeln("");
+
+
+
 
 
 document.writeln("  <tr>");
--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -28,11 +28,16 @@ document.writeln(" ");
 
 
 
-
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>081期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【风雨电】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
 
 
 document.writeln("  <tr>");
-document.writeln("    <td>080期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨电风】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>080期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨电<span style=\'background-color: #FFFF00\'>风</span>】</font>开<font color=\'#ac6a00\'>兔38</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

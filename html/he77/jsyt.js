646f3c0baec3ef5892ea3cc3c8559c0e2bf00609
xm01 <xm01@qq.com>
2024-03-30 00:54:30 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -13,12 +13,38 @@ document.writeln("");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>089期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【3333头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>090期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
-document.writeln("   ");
+document.writeln("");
+
+
 
 
 document.writeln("");

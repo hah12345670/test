c7ccb63c91b0069521428e7997d914d278c9c7a3
xm01 <xm01@qq.com>
2024-04-24 10:55:04 +0800

    1

--- html/bbs/yy4wzt.js
+++ html/bbs/yy4wzt.js
@@ -20,7 +20,7 @@ document.writeln("");
 document.writeln("<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'41\'><tbody><tr>");
 document.writeln("	<td width=100% height=41><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
-document.writeln("114期<font color=\'#FF00FF\'>（牛鸡龙+5293尾）</font>开？00</font></b></font></table>");
+document.writeln("115期<font color=\'#FF00FF\'>（马兔羊+8971尾）</font>开？00</font></b></font></table>");
 document.writeln("		<!--结束-->");
 
 document.writeln("");

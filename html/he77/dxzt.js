27f66dc8d5d12c72b5f71d3253610a32e89d088b
xm01 <xm01@qq.com>
2024-05-01 01:11:00 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -30,11 +30,16 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>122期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
 
 
 document.writeln("  <tr>");
-document.writeln("    <td>121期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>121期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>兔02</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

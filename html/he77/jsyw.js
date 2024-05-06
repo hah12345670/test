99e359b3c6dc153bfcbc9c2c2d5b5890096dadfc
xm01 <xm01@qq.com>
2024-05-06 08:44:27 +0800

    1

--- html/he77/jsyw.js
+++ html/he77/jsyw.js
@@ -34,13 +34,18 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>127期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【2222尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>126期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【6666尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>126期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【6666尾】</font>开<font color=\'#ac6a00\'>狗07</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 
--- html/he77/qqsh.js
+++ html/he77/qqsh.js
@@ -17,11 +17,15 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>127期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书画琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
 
 
 document.writeln("  <tr>");
-document.writeln("    <td>126期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【画琴棋】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>126期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【画琴<span style=\'background-color: #FFFF00\'>棋</span>】</font>开<font color=\'#ac6a00\'>狗07</font>准</td>");
 document.writeln("  </tr>");
 
 

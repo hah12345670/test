f8f90fbeb74af9fe80f182f561fd259afcf570e1
xm01 <xm01@qq.com>
2024-06-07 07:50:58 +0800

    1

--- html/he77/lengma.js
+++ html/he77/lengma.js
@@ -10,7 +10,7 @@ document.writeln("</style>");
 document.writeln("<div class=\'ipi\'>九點半<font color=\'#FFFF00\'>【解特码诗】</font></div>");
 document.writeln("<body>");
 document.writeln("");
-document.writeln("<!--特码诗开始 --");
+document.writeln("<!--特码诗开始 -->");
 document.writeln("<body>");
 document.writeln("");
 document.writeln("");
@@ -31,7 +31,7 @@ document.writeln("<table style=border-collapse:collapse border=1 width=100% bgco
 document.writeln("<tr>");
 document.writeln("<td width=100% height=40><p align=center><font face=\'楷体\'><b><font size=\'4\'>158期正版特码诗</font></b></font><br><font face=\'楷体\'><b><font color=\'#FF3300\' size=\'4\'>春入横塘摇浅浪，雪天蓦地忽闻香。</font></b></font><br>");
 document.writeln("<font face=\'楷体\'><b>");
-document.writeln("<font color=\'#FF3300\' size=\'4\'>解:关注九點半</font></b></font></table>");
+document.writeln("<font color=\'#FF3300\' size=\'4\'>解:特码开鸡20</font></b></font></table>");
 document.writeln("");
 document.writeln("");
 document.writeln("<!--特码诗开始 -->");

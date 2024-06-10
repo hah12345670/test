21a0aa1c9ed837d42376c9e8036151daaa77a64e
xm01 <xm01@qq.com>
2024-06-10 08:56:22 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -21,11 +21,14 @@ document.writeln("");
 
 
 
-
+document.writeln("  <tr>");
+document.writeln("    <td>162期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("  <tr>");
-document.writeln("    <td>161期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>161期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>马35</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

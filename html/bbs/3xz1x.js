c2e6ca1a3c06e47253cb2870dc7bb1706824b60d
xm01 <xm01@qq.com>
2024-07-08 23:07:34 +0800

    1

--- html/bbs/3xz1x.js
+++ html/bbs/3xz1x.js
@@ -35,17 +35,17 @@ document.writeln("		<font size=\"5\">（余生有钱）推荐一肖三码</font>
 document.writeln("	<div class=\'bizhong333-box\'>");
 document.writeln("    	<div class=\'bizhong333-l\' style=\"background-color: #FFFFFF\">");
 document.writeln("        	<ul>");
-document.writeln(" 	            <li><font size=\"2\">190期必中18码：</font><font color=\'#FF0000\' size=\'2\'>22.34.46.02.14.38.19.31.43.01.37.49.09.21.33.06.30.42</font></li>");
-document.writeln("            	<li>190期必中⑨码：<font color=\'#FF0000\' style=\"font-size: 14pt\">22.34.46.02.14.38.19.31.43</font></li>");
-document.writeln("            	<li>190期必中①肖：<font color=\'#FF0000\' size=\'5\'>（羊-22.34.46）</font></li>");
-document.writeln("            	<li>190期必中③肖：<font color=\'#FF0000\' size=\'4\'>羊兔狗</font></li>");
-document.writeln("            	<li>190期必中⑥肖：<font color=\'#FF0000\' size=\'4\'>羊兔狗龙猴猪</font></li>");
+document.writeln(" 	            <li><font size=\"2\">191期必中18码：</font><font color=\'#FF0000\' size=\'2\'>06.18.30.09.21.33.12.36.48.10.22.34.08.20.32.04.16.40</font></li>");
+document.writeln("            	<li>191期必中⑨码：<font color=\'#FF0000\' style=\"font-size: 14pt\">06.18.30.09.21.33.12.36.48</font></li>");
+document.writeln("            	<li>191期必中①肖：<font color=\'#FF0000\' size=\'5\'>（猪-06.18.30）</font></li>");
+document.writeln("            	<li>191期必中③肖：<font color=\'#FF0000\' size=\'4\'>猪猴蛇</font></li>");
+document.writeln("            	<li>191期必中⑥肖：<font color=\'#FF0000\' size=\'4\'>猪猴蛇羊鸡牛</font></li>");
 document.writeln("        </div>");
 document.writeln("        </div>");
 document.writeln("");
 document.writeln("</div>");
 document.writeln("<!--必中每期开始-->");
-document.writeln("");
+
 document.writeln("");
 document.writeln("<!--必中每期开始-->");
 document.writeln("<div class=\'bizhong333\'>");

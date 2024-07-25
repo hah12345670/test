f67ade8c6bcb60343b424756de1883e4586e952d
xm01 <xm01@qq.com>
2024-07-25 08:12:51 +0800

    1

--- html/bbs/ptgk.js
+++ html/bbs/ptgk.js
@@ -16,11 +16,16 @@ document.writeln("			<a target=\'_blank\' href=\'https://j.1777tz.com:1777/\' st
 document.writeln("<!--推广结束-->");
 document.writeln("");
 document.writeln("");
+document.writeln("<!--开始-->");
+document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'46\'><tbody><tr>");
+document.writeln("	<td width=100% height=46><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
+document.writeln("207期<font color=\'#FF0000\'>平狗→狗马(2连)→07.31.23.47(连)</font></font></b></font></table>");
+document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'46\'><tbody><tr>");
 document.writeln("	<td width=100% height=46><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
-document.writeln("206期<font color=\'#FF0000\'>平鼠→鼠狗(2连)→05.41.07.19(连)</font></font></b></font></table>");
+document.writeln("206期<font color=\'#FF0000\'>平鼠→鼠<span style=\'background-color: #FFFF00\'>狗</span>(2连)→05.41.07.19(连)</font></font></b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("<!--开始-->");

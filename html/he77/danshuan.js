73a77123b74ca5cb716e0074f253abed00e9faf9
xm01 <xm01@qq.com>
2024-07-31 10:33:34 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -55,7 +55,12 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>212期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>213期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("</tr>");
+document.writeln("");
+document.writeln("")
+document.writeln("<tr>");
+document.writeln("<td><font color=\'#008000\'>212期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：龙49</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")

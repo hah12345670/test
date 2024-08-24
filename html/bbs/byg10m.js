31989e6aa544470ff15437603ad7c86afc2b4ca6
xm01 <xm01@qq.com>
2024-08-24 12:36:04 +0800

    1

--- html/bbs/byg10m.js
+++ html/bbs/byg10m.js
@@ -3,7 +3,7 @@ document.writeln("<div style=\"display:none\">");
 document.writeln("</div>");
 document.writeln("");
 document.writeln("");
-
+document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("");
@@ -12,7 +12,7 @@ document.writeln("");
 document.writeln("    <!--62开始结束--> ");
 document.writeln("<table border=1 width=100% bgcolor=#ffffff height=\"55\"><tbody>");
 document.writeln("<td style=\'border:10px double #00f; height: 49px;\' bgcolor=#0000FF><p align=center>");
-document.writeln("<b><font color=\'#FFFFFF\' face=\'楷体\' style=\'font-size: 16pt\'>【神经】(3肖4码)62827b.com</font></b></td></table>");
+document.writeln("<b><font color=\'#FFFFFF\' face=\'楷体\' style=\'font-size: 16pt\'>【大亨送彩】(二肖四码)</font></b></td></table>");
 document.writeln("");
 document.writeln("	");
 document.writeln("");
@@ -36,41 +36,25 @@ document.writeln("		");
 document.writeln("    	<tr>");
 document.writeln("      <td height=40 bgcolor=\"#FFFFFF\" >");
 document.writeln("");
-document.writeln("<p align=\"center\"><b><font size=\"5\">");
+document.writeln("<p align=\"center\"><b><font size=\"4\">");
 document.writeln("");
-document.writeln("226期猴<span style=\"background-color: #FFFF00\">羊</span>龙<br>");
-document.writeln("08.20.30.42<br><br>");
+document.writeln("235期:【大亨送彩】二肖四码开:<font color=\"#FF00FF\">蛇24</font>中		");
+document.writeln("<br>【鸡<span style=\"background-color: #FFFF00\">蛇</span>】【11.23.25.49】<br><br>");
 document.writeln("");
-document.writeln("227期<span style=\"background-color: #FFFF00\">鸡</span>羊狗<br>");
-document.writeln("04.16.15.27<br><br>");
-
 document.writeln("");
-document.writeln("229期马<span style=\"background-color: #FFFF00\">鼠</span>羊<br>");
-document.writeln("07.43.18.42<br><br>");
 document.writeln("");
-document.writeln("230期羊牛<span style=\"background-color: #FFFF00\">龙</span><br>");
-document.writeln("18.30.24.48<br><br>");
-
-
-document.writeln("				232期牛龙<span style=\'background-color: #FFFF00\'>兔</span><br>");
-document.writeln("				24.48.05.41<br><br>");
-
-
-
-document.writeln("				233期猴鸡鼠<br>");
-document.writeln("				03.15.<span style=\'background-color: #FFFF00\'>23</span>.47<br><br>");
-
-
-document.writeln("				235期鼠狗<span style=\'background-color: #FFFF00\'>蛇</span><br>");
-document.writeln("				18.30.04.16<br><br>");
-
-
-document.writeln("				236期猴龙鸡<br>");
-document.writeln("				11.23.05.41<br><br>");
-
-
-
-
+document.writeln("");
+document.writeln("236期:【大亨送彩】二肖四码开:<font color=\"#FF00FF\">虎03</font>中		");
+document.writeln("<br>【<span style=\"background-color: #FFFF00\">虎</span>羊】【35.47.08.44】<br><br>");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("237期:【大亨送彩】二肖四码开:<font color=\"#FF00FF\">？00</font>中		");
+document.writeln("<br>【62827b.com 第一个高手榜第四贴已更新】<br>");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
 document.writeln("</font>");
 document.writeln("");
 document.writeln("</p>");

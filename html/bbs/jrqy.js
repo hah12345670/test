bd10e7391e481a04880542f94c24158dc1548cc5
xm01 <xm01@qq.com>
2024-08-28 16:06:46 +0800

    1

--- html/bbs/jrqy.js
+++ html/bbs/jrqy.js
@@ -14,7 +14,7 @@ document.writeln(".bizhong1234 img {width:100%; border-collapse: collapse; font-
 document.writeln("</style>");
 document.writeln("<div class=\'bizhong1234 clearfix\'>");
 document.writeln("    <div class=\'bizhong1234-box action-bz\'>");
-document.writeln("<img src=\'/img/jrqy239.jpg\' id=\'jrqypicx\'>");
+document.writeln("<img src=\'/img/jrqy240.jpg\' id=\'jrqypicx\'>");
 document.writeln("    </div>");
 document.writeln("    ");
 document.writeln("</div>");
@@ -25,7 +25,7 @@ document.writeln(" <span class=\'bizhong1234-btn bizhong1234-next\' onclick=\'ne
 document.writeln("</div>");
 document.writeln("<script>");
 document.writeln("    var picArrsbx=new Array(");
-
+document.writeln("\'/img/jrqy240.jpg\',");
 document.writeln("\'/img/jrqy239.jpg\',");
 document.writeln("\'/img/jrqy238.jpg\',");
 document.writeln("\'/img/jrqy237.jpg\',");

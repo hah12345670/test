adc19283c7d1e5ffb5d4fd511375837962cfaeda
xm01 <xm01@qq.com>
2024-08-31 16:20:12 +0800

    1

--- html/bbs/4leix.js
+++ html/bbs/4leix.js
@@ -1,6 +1,94 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
-<HTML><HEAD><TITLE>Service Unavailable</TITLE>
-<META HTTP-EQUIV="Content-Type" Content="text/html; charset=us-ascii"></HEAD>
-<BODY><h2>Service Unavailable</h2>
-<hr><p>HTTP Error 503. The service is unavailable.</p>
-</BODY></HTML>
+﻿document.writeln("");
+document.writeln("");
+document.writeln("<style>");
+document.writeln("");
+document.writeln("<style>");
+document.writeln("bjbj {");
+document.writeln("background-color:#ffffff;");
+document.writeln("}");
+document.writeln(".a1 td {");
+document.writeln("	border-bottom: solid 1px #c0c0c0;");
+document.writeln("}");
+document.writeln("</style>");
+document.writeln("</style>");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("<body class=\'bjbj\'>");
+document.writeln("");
+document.writeln("");
+document.writeln("	<div class=\'a1\'>");
+document.writeln("<table width=\'100%\' id=\'table1\' cellspacing=\'0\' cellpadding=\'0\'>");
+document.writeln("  <tbody>");
+document.writeln("<td style=\'text-align:center\' height=\'60\'>");
+document.writeln("<table border=1 width=100% bgcolor=#ffffff><tbody>");
+document.writeln("<td style=\'border:10px double #00f; height: 50px;\' bgcolor=#0000FF><p align=center>");
+document.writeln("<b><font color=\'#FFFF00\' face=\'楷体\' style=\'font-size: 18pt\'>62827.com</font><font color=\'#FFFFFF\' face=\'楷体\' style=\'font-size: 18pt\'>『四类肖』</font></b></table>");
+document.writeln("");
+document.writeln("");
+document.writeln("<!--推广开始-->");
+document.writeln("<tr><td><a target=\'_blank\' href=\'https://j.1777tz.com:1777\' style=\'text-decoration: none\'>");
+document.writeln("<p align=\'center\'>");
+document.writeln("	<img src=/images/yh.gif style=\'width:20px\' height=\'19\'><b><font face=\'楷体\' color=\'#0000FF\' style=\'font-size: 16pt\'>点击投注六合彩</span><span style=font-family:楷体;font-size:17pt;><font color=\'#FF0000\'>1777.CC</font></span></b></p></a></td></tr>");
+document.writeln(" ");
+document.writeln("");
+document.writeln("   <!--开始-->");
+document.writeln("    ");
+document.writeln("	<tr>");
+document.writeln("      <td height=42 bgcolor=\'#FFFFFF\' >");
+document.writeln("		<p align=\'center\'>");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		");
+document.writeln("		<b>");
+document.writeln("		");
+document.writeln("		<font style=\'font-size: 14pt\' color=\'#FF0000\'>君王【龙虎】恶人【鼠猴】<br>四大美女【兔蛇羊鸡】<br>四大家臣【牛马狗猪】</span></font></b></td>");
+document.writeln("    </tr>");
+document.writeln(" <!--结束--> ");
+document.writeln("");
+document.writeln("");
+
+
+document.writeln("");
+document.writeln("<!--开始-->");
+document.writeln("    ");
+document.writeln("	<tr>");
+document.writeln("      <td height=42 bgcolor=\'#FFFFFF\' >");
+document.writeln("		<p align=\'center\'><b>");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		");
+document.writeln("		<font style=\'font-size: 14pt\'>244期四类<font color=\'#008000\'>【君王美女】</font></font><span style=\'font-size: 14pt\'>开<font color=\'#808000\'>？00</font>准</span></font></b></td>");
+document.writeln("    </tr>");
+document.writeln(" <!--结束--> ");
+
+document.writeln("");
+document.writeln("<!--开始-->");
+document.writeln("    ");
+document.writeln("	<tr>");
+document.writeln("      <td height=42 bgcolor=\'#FFFFFF\' >");
+document.writeln("		<p align=\'center\'><b>");
+document.writeln("		<font face=\'微软雅黑\'>");
+document.writeln("		");
+document.writeln("		<font style=\'font-size: 14pt\'>243期四类<font color=\'#008000\'>【<span style=\'background-color: #FFFF00\'>君王</span>家臣】</font></font><span style=\'font-size: 14pt\'>开<font color=\'#808000\'>龙01</font>准</span></font></b></td>");
+document.writeln("    </tr>");
+document.writeln(" <!--结束--> ");
+
+
+
+
+
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("<!--推广开始-->");
+document.writeln("<tr><td><a target=\'_blank\' href=\'https://xn--oecalb2e.xn--hdcn9ajb1dyeua6etcq8g3b.xn--gecrj9c:8443/#49029\' style=\'text-decoration: none\'>");
+document.writeln("<p align=\'center\'>");
+document.writeln("	<img src=https://hjyjthjy.www661161c.com:8443/images/yh.gif style=\'width:20px\' height=\'19\'><b><font face=\'楷体\' color=\'#0000FF\' size=\'5\'>跑狗论坛<font color=\'#FF0000\'>37197c.com</font></font></b></p></a></td></tr>");
+document.writeln(" ");
+document.writeln("");
+document.writeln("");
+document.writeln("  </tbody>");
+document.writeln("</table>");
+document.writeln(" </div>");
+document.writeln("    ");
+document.writeln("</body>");

30e4ba051df151decaaccff84abb6f5c6ba2862e
xm01 <xm01@qq.com>
2024-09-03 23:36:36 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -57,7 +57,12 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>247期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>248期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("</tr>");
+document.writeln("");
+document.writeln("")
+document.writeln("<tr>");
+document.writeln("<td><font color=\'#008000\'>247期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：狗31</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")

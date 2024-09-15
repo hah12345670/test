689694174843389e4d9a2c86e662a65e523ac50d
xm01 <xm01@qq.com>
2024-09-16 00:36:08 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -58,7 +58,12 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>259期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>260期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("</tr>");
+document.writeln("");
+document.writeln("")
+document.writeln("<tr>");
+document.writeln("<td><font color=\'#008000\'>259期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：鼠29</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")
--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -13,13 +13,11 @@ document.writeln("");
 
 
 
-
-
-
 document.writeln("  <tr>");
-document.writeln("    <td>259期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>260期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
+
 
 document.writeln("  <tr>");
 document.writeln("    <td>258期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>牛40</font>准</td>");

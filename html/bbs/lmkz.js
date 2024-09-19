083ae2003b81139f4af9caf5ec7da3a0d13ffea1
xm01 <xm01@qq.com>
2024-09-19 17:04:32 +0800

    1

--- html/bbs/lmkz.js
+++ html/bbs/lmkz.js
@@ -25,6 +25,34 @@ document.writeln("	<div class=\'a1\'>");
 document.writeln("<table width=\'100%\' id=\'table1\' cellspacing=\'0\' cellpadding=\'0\' height=\'200\'>");
 document.writeln("  <tbody>");
 
+document.writeln("");
+document.writeln("                                        <!--开始-->");
+document.writeln("	<tr>");
+document.writeln("      <td align=\'left\' height=47 bgcolor=\'#FFFFFF\' ><p>");
+document.writeln("		<span style=\'font-weight: 700\'><font face=\'微软雅黑\'>263期：【职业玩家】破解龙门客栈</font><br>");
+document.writeln("		<font face=\'华文楷体\'><font color=\'#FF0000\' style=\'font-size: 12pt; font-weight:700\'>掌柜：【难解难分】<br>小二：【客官，二位里面请】<br>");
+document.writeln("		客官：【一壶碧螺春，江豆粒，南瓜饼】</font></font></font></span></td>");
+document.writeln("    </tr>");
+document.writeln("    ");
+document.writeln("	<tr>");
+document.writeln("      <td align=\'left\' height=34 bgcolor=\'#FFFFFF\' ><p><b>");
+document.writeln("		<font color=\'#000000\' style=\'font-size: 12pt\' face=\'微软雅黑\'>解：难解难分，解鼠龙兔鸡狗虎。</font></td>");
+document.writeln("    </tr>");
+document.writeln("	<tr>");
+document.writeln("      <td align=\'left\'  bgcolor=\'#FFFFFF\' ><p>");
+document.writeln("		<font face=\'华文楷体\' style=\'font-size: 14pt; font-weight:700\' color=\'#008000\'>");
+document.writeln("				综合特肖：鼠龙兔鸡狗虎</font></td>");
+document.writeln("    </tr>");
+document.writeln("	<tr>");
+document.writeln("      <td align=\'left\' height=28 bgcolor=\'#0000FF\' ><p><b>");
+document.writeln("		<font face=\'微软雅黑\' size=\'2\'>");
+document.writeln("		<a target=\'_blank\' href=\'https://j.1999zx.com:1999/\' style=\'text-decoration: none\'>");
+document.writeln("		<font color=\'#FFFFFF\'>关注资料网</font></font><font color=\'#FFFF00\' size=\'4\' face=\'微软雅黑\'>62827a.com</font><font color=\'#FFFFFF\' face=\'微软雅黑\' size=\'2\'>最早看料</font></a><font face=\'微软雅黑\' size=\'2\'><font color=\'#FFFFFF\'> </font>  ");
+document.writeln("		</font>  ");
+document.writeln("		</b> </td>");
+document.writeln("    </tr>");
+document.writeln("<!--开始-->");
+
 
 
 document.writeln("");
@@ -43,7 +71,7 @@ document.writeln("    </tr>");
 document.writeln("	<tr>");
 document.writeln("      <td align=\'left\'  bgcolor=\'#FFFFFF\' ><p>");
 document.writeln("		<font face=\'华文楷体\' style=\'font-size: 14pt; font-weight:700\' color=\'#008000\'>");
-document.writeln("				综合特肖：猴牛虎龙兔鸡</font></td>");
+document.writeln("				综合特肖：猴牛虎龙兔<span style=\'background-color: #FFFF00\'>鸡</span></font></td>");
 document.writeln("    </tr>");
 document.writeln("	<tr>");
 document.writeln("      <td align=\'left\' height=28 bgcolor=\'#0000FF\' ><p><b>");

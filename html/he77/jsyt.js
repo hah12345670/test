a3a716fccf91b5e745161e3dd6334c9784b4c130
xm01 <xm01@qq.com>
2024-09-30 02:27:05 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -29,14 +29,19 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>263期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【4444头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>273期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>273期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>鼠41</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("   ");
 
--- html/he77/jsyw.js
+++ html/he77/jsyw.js
@@ -27,14 +27,13 @@ document.writeln("");
 
 
 
-
-
-
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>273期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【1111尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>263期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【4444尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
+
+
 
 
 document.writeln("");

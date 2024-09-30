d1bff3f5a7376c813ac45e3bb698cdd160af7528
xm01 <xm01@qq.com>
2024-09-30 09:12:08 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -58,13 +58,13 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>274期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【？？？】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>274期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")
 
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>273期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>273期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：鼠41</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")

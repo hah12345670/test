8aa0f62c746f148cc01928d0d2db24b98215b301
xm01 <xm01@qq.com>
2024-10-09 23:15:55 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -27,13 +27,19 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>284期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("   ");
 
 document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>283期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>283期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>马47</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

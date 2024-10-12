83442f3ce841b998989697464c6536119dc3785b
xm01 <xm01@qq.com>
2024-10-13 02:57:51 +0800

    1

--- html/he77/cxqd.js
+++ html/he77/cxqd.js
@@ -18,10 +18,29 @@ document.writeln("");
 
 
 
+
+
+
+
+
+
+
+
+
+
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>287期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【冬秋春】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
+document.writeln(" ");
+
+
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>286期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【秋夏冬】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>286期<font color=\'#ac6a00\'>春夏秋冬</font><font color=\'#0000FF\'>【<span style=\'background-color: #FFFF00\'>秋</span>夏冬】</font>开<font color=\'#ac6a00\'>猴45</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");
--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -24,9 +24,17 @@ document.writeln("");
 
 document.writeln("");
 document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>287期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【3333头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("   ");
+
+
+document.writeln("");
+document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>286期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>286期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>猴45</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

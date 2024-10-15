9da3b90ababb698b73e00fe5063ca229ef248e10
xm01 <xm01@qq.com>
2024-10-15 11:01:41 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -59,7 +59,12 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>288期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>289期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("</tr>");
+document.writeln("");
+document.writeln("")
+document.writeln("<tr>");
+document.writeln("<td><font color=\'#008000\'>288期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：兔26</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")

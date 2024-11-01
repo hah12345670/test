9c2a879bd0d99d55daca48ff662978677d2900cf
xm01 <xm01@qq.com>
2024-11-01 11:08:37 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -60,16 +60,17 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>306期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【更新中】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>307期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【？？？】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>305期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>306期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("")
+document.writeln("");
 document.writeln("<tr>");
 document.writeln("<td><font color=\'#008000\'>304期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：鼠17</font>准</td>");
 document.writeln("</tr>");
--- html/he77/jsyw.js
+++ html/he77/jsyw.js
@@ -14,12 +14,17 @@ document.writeln("");
 
 
 
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>306期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【7777尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>305期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【6666尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>305期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【6666尾】</font>开<font color=\'#ac6a00\'>鸡32</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

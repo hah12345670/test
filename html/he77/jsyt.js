bf81341bede5a9b78850ac33bd39c1108e6b546a
xm01 <xm01@qq.com>
2024-11-07 09:44:48 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -27,13 +27,18 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>312期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【2222头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("   ");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>311期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>311期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>猴21</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

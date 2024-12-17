3a54ead5f818d2cf65872263f05db5b732e6fe92
xm01 <xm01@qq.com>
2024-12-17 09:59:09 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -32,13 +32,18 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>352期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("   ");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>351期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>351期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>虎27</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

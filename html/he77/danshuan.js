5ab86451fff954d20f559ca781bf480fb7c8e382
xm01 <xm01@qq.com>
2025-01-16 10:19:48 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -68,10 +68,11 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>015期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>016期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("");
+
 document.writeln("<tr>");
 document.writeln("<td><font color=\'#008000\'>014期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：虎27</font>准</td>");
 document.writeln("</tr>");

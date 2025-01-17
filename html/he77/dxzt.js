a76e8e2d137d5377e7b38c22fbdc16c9b2fd9185
xm01 <xm01@qq.com>
2025-01-17 11:32:17 +0800

    1

--- html/he77/dxzt.js
+++ html/he77/dxzt.js
@@ -14,8 +14,37 @@ document.writeln("");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.writeln("  <tr>");
-document.writeln("    <td>016期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>017期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+
+
+document.writeln("  <tr>");
+document.writeln("    <td>016期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>猴21</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 
@@ -59,24 +88,6 @@ document.writeln("    <td>008期<font color=\'#ac6a00\'>大小中特</font><font
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");
-
-
-
-
-
-
-document.writeln("  <tr>");
-document.writeln("    <td>005期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【小小小】</font>开<font color=\'#ac6a00\'>鸡08</font>准</td>");
-document.writeln("  </tr>");
-document.writeln("");
-
-
-document.writeln("  <tr>");
-document.writeln("    <td>004期<font color=\'#ac6a00\'>大小中特</font><font color=\'#0000FF\'>【大大大】</font>开<font color=\'#ac6a00\'>猴33</font>准</td>");
-document.writeln("  </tr>");
-document.writeln("");
-
-
 
 
 

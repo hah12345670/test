e1e4ea01d2d5e2583d439f02840770dd349912ac
xm01 <xm01@qq.com>
2025-01-18 00:15:57 +0800

    1

--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -33,7 +33,12 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>018期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 
@@ -41,7 +46,7 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>017期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>017期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>兔38</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

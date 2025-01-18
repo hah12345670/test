5b6db4cc9b786dc6b630eb0ceaa2780bdb9bab90
xm01 <xm01@qq.com>
2025-01-18 13:26:51 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -68,11 +68,15 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>017期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>018期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("");
-
+document.writeln("<tr>");
+document.writeln("<td><font color=\'#008000\'>017期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：兔38</font>准</td>");
+document.writeln("</tr>");
+document.writeln("");
+document.writeln("");
 document.writeln("<tr>");
 document.writeln("<td><font color=\'#008000\'>014期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【单单单】</font><font color=\'#0000FF\'>开：虎27</font>准</td>");
 document.writeln("</tr>");

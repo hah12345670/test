5154668e360c7924949ba132d65ae24a8d879f1b
xm01 <xm01@qq.com>
2025-01-21 01:11:46 +0800

    1

--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -27,14 +27,15 @@ document.writeln(" ");
 
 
 
-
-
-
-
+document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>020期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雷电风】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>021期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨风雷】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
+document.writeln("");
+
+
+
 
 
 

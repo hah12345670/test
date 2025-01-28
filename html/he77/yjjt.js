4518d720cad56c2311f1dd00af6662dd5b17debd
xm <xm@qq.com>
2025-01-29 01:00:41 +0800

    1

--- html/he77/yjjt.js
+++ html/he77/yjjt.js
@@ -57,7 +57,15 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("<tr><td align=\'left\' height=60 style=\'border-style: solid; border-width: 1px\'><b>");
+document.writeln("<font style=\'font-size: 13pt\'>");
+document.writeln("029期玄机诗句<font color=\'#FF0000\'>【搅海翻江多摆布，劈邪除妖向前越。】</font>");
+document.writeln("<br><font color=\'#0000FF\'>解析：搅海翻江多摆布，搅海翻江，龙蛇猴马；劈邪除妖向前越，劈邪除妖，牛鬼蛇神，牛兔蛇。</font>");
+document.writeln("<br><font color=\'#008000\'>解特：龙蛇猴马牛兔");
+document.writeln("</font></font></b></td></tr>");
+document.writeln("");
+document.writeln("");
 
 
 document.writeln("");
@@ -66,7 +74,7 @@ document.writeln("<tr><td align=\'left\' height=60 style=\'border-style: solid; 
 document.writeln("<font style=\'font-size: 13pt\'>");
 document.writeln("028期玄机诗句<font color=\'#FF0000\'>【冲天香阵透长安，满城尽带黄金甲】</font>");
 document.writeln("<br><font color=\'#0000FF\'>解析：天解天肖，兔马猴猪牛龙。</font>");
-document.writeln("<br><font color=\'#008000\'>解特：兔马猴猪牛龙");
+document.writeln("<br><font color=\'#008000\'>解特：兔马猴猪<span style=\'background-color: #FFFF00\'>牛</span>龙");
 document.writeln("</font></font></b></td></tr>");
 document.writeln("");
 

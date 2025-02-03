334d0b783510b24b8ac46d7210ddd3ba8ebbf1c3
xm <xm@qq.com>
2025-02-03 10:32:07 +0800

    1

--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -38,13 +38,18 @@ document.writeln(" ");
 
 
 
-
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>034期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雷风电】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+document.writeln("");
 
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>033期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【风雷雨】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>033期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【风雷<span style=\'background-color: #FFFF00\'>雨</span>】</font>开<font color=\'#ac6a00\'>马48</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");
--- html/he77/jsyt.js
+++ html/he77/jsyt.js
@@ -33,13 +33,18 @@ document.writeln("");
 
 
 
-
+document.writeln("");
+document.writeln("");
+document.writeln("  <tr>");
+document.writeln("    <td>034期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【1111头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>033期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>033期<font color=\'#ac6a00\'>绝杀一头</font><font color=\'#0000FF\'>【0000头】</font>开<font color=\'#ac6a00\'>马48</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

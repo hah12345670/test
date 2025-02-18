8f5d61486a7fecead30748705b2591b99297ca70
xm01 <xm01@qq.com>
2025-02-18 09:53:13 +0800

    1

--- html/he77/yjjt.js
+++ html/he77/yjjt.js
@@ -59,19 +59,17 @@ document.writeln("");
 
 
 
-
-
-
-document.writeln("");
 document.writeln("");
 document.writeln("<tr><td align=\'left\' height=60 style=\'border-style: solid; border-width: 1px\'><b>");
 document.writeln("<font style=\'font-size: 13pt\'>");
-document.writeln("048期玄机诗句<font color=\'#FF0000\'>【金屋藏娇定富贵，不是蛇头哭衰衰。】</font>");
-document.writeln("<br><font color=\'#0000FF\'>解析：金屋藏娇定富贵，金屋是猴鸡。藏娇，是女肖兔、蛇、羊、鸡、猪。</font>");
-document.writeln("<br><font color=\'#008000\'>解特：猴兔蛇羊鸡猪");
+document.writeln("049期玄机诗句<font color=\'#FF0000\'>【四季勤耕下农田，如今借得东风力。】</font>");
+document.writeln("<br><font color=\'#0000FF\'>解析：勤耕下农田，牛，也只常年忙碌，马狗；东风力，东方肖，虎兔龙，风肖，龙虎兔。</font>");
+document.writeln("<br><font color=\'#008000\'>解特：龙虎兔牛马狗");
 document.writeln("</font></font></b></td></tr>");
 document.writeln("");
 document.writeln("");
+
+
 
 
 

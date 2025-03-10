a416f968dec4c30a0b756488b0d66d97988cec2d
xm01 <xm01@qq.com>
2025-03-11 00:32:37 +0800

    1

--- html/he77/danshuan.js
+++ html/he77/danshuan.js
@@ -37,12 +37,14 @@ document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("");
+document.writeln("");
 document.writeln("<tr>");
-document.writeln("<td><font color=\'#008000\'>069期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
+document.writeln("<td><font color=\'#008000\'>070期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：？00</font>准</td>");
 document.writeln("</tr>");
 document.writeln("");
 document.writeln("");
 document.writeln("");
+
 document.writeln("<tr>");
 document.writeln("<td><font color=\'#008000\'>068期</font><img src=\'/yjjy/tz/images/am.png\'><font color=\'#FF0000\'>【双双双】</font><font color=\'#0000FF\'>开：狗44</font>准</td>");
 document.writeln("</tr>");

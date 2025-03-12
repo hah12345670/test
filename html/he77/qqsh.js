457619b8857ef8312e9e15d99a6b750e1c009ed7
xm01 <xm01@qq.com>
2025-03-12 10:21:43 +0800

    1

--- html/he77/qqsh.js
+++ html/he77/qqsh.js
@@ -18,16 +18,11 @@ document.writeln(" <tr>    <td style=\'height: 28px\'><font color=\'#0B6121\'>�
 
 
 
-
-
-
-
 document.writeln("");
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>070期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【棋书琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>071期<font color=\'#ac6a00\'>琴棋书画</font><font color=\'#0000FF\'>【书棋琴】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
 document.writeln("  </tr>");
-document.writeln("");
 
 
 

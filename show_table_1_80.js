f8955b0b1f1250ec8ffcb66cc60e8f9ba93a31df
xm01 <xm01@qq.com>
2025-05-18 16:44:56 +0800

    1

--- show_table_1_80.js
+++ show_table_1_80.js
@@ -1,5 +1,5 @@
-// 显示表格
-function show_table_1to80(inputNumbers, nid = 'showdata1') {
+// 显示表格 最近开奖号码
+function show_table_1to80_num(inputNumbers, nid = 'showdata1') {
 	// let inputNumbers = [];
 	let highlighted = new Set(inputNumbers);
 	let count = 1;
@@ -16,6 +16,39 @@ function show_table_1to80(inputNumbers, nid = 'showdata1') {
 				str1 += '<td class="'+extraClass.trim()+'"><div class="highlight">'+displayNumber+'</div></td>';
 			} else {
 				str1 += '<td class="'+extraClass.trim()+'">'+displayNumber+'</td>';
+			}
+			count++;
+		}
+		str1 += '</tr>';
+	}
+	str1 += '</tbody></table>';
+	document.getElementById(nid).innerHTML = str1;
+}
+
+// 推荐号码 最近开奖号码
+function show_table_1to80_numtj(kjNumbers, tjNumbers, nid = 'showdata1') {
+	let kjnumed = new Set(kjNumbers);
+	let tjnumed = new Set(tjNumbers);
+	let count = 1;
+	let str1 = '<table id="showtable"><caption></caption><tbody>';
+	for(let i=0;i<8;i++) {
+		str1 += '<tr>';
+		for(let j=0;j<10;j++) {
+			let extraClass = '';
+			if(i === 4) extraClass += ' thick-top';
+			if(j === 5) extraClass += ' thick-left';
+
+			let displayNumber = count < 10 ? '0'+count : count;
+			if(kjnumed.has(count) && tjnumed.has(count)){
+				str1 += '<td class="'+extraClass.trim()+'"><div class="tjkjnum">'+displayNumber+'</div></td>';
+			} else {
+				if(kjnumed.has(count)){
+					str1 += '<td class="'+extraClass.trim()+'"><div class="highlight">'+displayNumber+'</div></td>';
+				} else if(tjnumed.has(count)){
+					str1 += '<td class="'+extraClass.trim()+'"><div class="tjnum">'+displayNumber+'</div></td>';
+				} else {
+					str1 += '<td class="'+extraClass.trim()+'">'+displayNumber+'</td>';
+				}
 			}
 			count++;
 		}

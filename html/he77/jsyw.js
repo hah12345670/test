7f21c884bf2720e9c1c0958037ab36b14c88c3e2
xm01 <xm01@qq.com>
2025-06-01 01:21:30 +0800

    1

--- html/he77/jsyw.js
+++ html/he77/jsyw.js
@@ -15,8 +15,33 @@ document.writeln("");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.writeln("  <tr>");
-document.writeln("    <td>151期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【1111尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>152期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【3333尾】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
+
+
+document.writeln("  <tr>");
+document.writeln("    <td>151期<font color=\'#ac6a00\'>绝杀一尾</font><font color=\'#0000FF\'>【1111尾】</font>开<font color=\'#ac6a00\'>龙38</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 

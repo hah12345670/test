27200e3f62e71577c3c308cec45a4880042d3bf1
xm01 <xm01@qq.com>
2025-06-08 09:43:13 +0800

    1

--- html/bbs/6z3wzt.js
+++ html/bbs/6z3wzt.js
@@ -9,14 +9,23 @@ document.writeln("");
 document.writeln("<table border=\"1\" width=\"100%\" id=\"table400923401\">");
 document.writeln("");
 
-
+document.writeln("	<!--gavc-->");
+document.writeln("	<tr>");
+document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\"> <b>");
+document.writeln("		<p align=\"left\">");
+document.writeln("		<font color=\"#000000\" face=\"微软雅黑\" style=\"font-size: 12pt\">");
+document.writeln("		159期;七尾：<font color=\"#FF0000\" face=\"微软雅黑\" size=\"4\">（1.6.8.2.5.4.7）</font><br>");
+document.writeln("		159期;四尾：<font color=\"#FF0000\" face=\"微软雅黑\" size=\"4\">（1.6.8.2）<BR>		");
+document.writeln("		===============</font></font></p></b></td>");
+document.writeln("	</tr>");
+document.writeln("	<!--结束-->	");
 
 document.writeln("	<!--gavc-->");
 document.writeln("	<tr>");
 document.writeln("		<td bgcolor=\"#FFFFFF\" height=\"38\"> <b>");
 document.writeln("		<p align=\"left\">");
 document.writeln("		<font color=\"#000000\" face=\"微软雅黑\" style=\"font-size: 12pt\">");
-document.writeln("		158期;七尾：<font color=\"#FF0000\" face=\"微软雅黑\" size=\"4\">（3.1.6.9.5.8.7）</font><br>");
+document.writeln("		158期;七尾：<font color=\"#FF0000\" face=\"微软雅黑\" size=\"4\">（3.1.6.9.<span style=\'background-color: #FFFF00\'>【5】</span>.8.7）</font><br>");
 document.writeln("		158期;四尾：<font color=\"#FF0000\" face=\"微软雅黑\" size=\"4\">（3.1.6.9）<BR>		");
 document.writeln("		===============</font></font></p></b></td>");
 document.writeln("	</tr>");

ab305acab1b7de7c893302f313102b2c52fbb2f3
xm01 <xm01@qq.com>
2025-06-17 13:50:30 +0800

    1

--- html/he77/fyld.js
+++ html/he77/fyld.js
@@ -25,12 +25,15 @@ document.writeln(" ");
 
 
 
-
+document.writeln("  <tr>");
+document.writeln("    <td>168期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨电风】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("  </tr>");
+document.writeln("");
 
 
 document.writeln("");
 document.writeln("  <tr>");
-document.writeln("    <td>167期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨风雷】</font>开<font color=\'#ac6a00\'>？00</font>准</td>");
+document.writeln("    <td>167期<font color=\'#ac6a00\'>风雨雷电</font><font color=\'#0000FF\'>【雨风<span style=\'background-color: #FFFF00\'>雷</span>】</font>开<font color=\'#ac6a00\'>鸡09</font>准</td>");
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");

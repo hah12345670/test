3ad69567654bf683ab497a6ad95039303df3be3d
xm01 <xm01@qq.com>
2025-09-07 09:30:43 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -106,7 +106,7 @@ document.writeln("    <td class=\'ymgg-tit1\' bgcolor=\"#FF0000\">");
 document.writeln("	<p align=\'center\'><font style=\"font-size: 16pt; font-weight: 700\">");
 document.writeln("	246-250期特码:（五期必中）</font><font color=\"#FFFF00\"><span style=\"font-size: 16pt\"><br>");
 document.writeln("	</span><font style=\"font-size: 18pt; font-weight: 700\">");
-document.writeln("	02.09.13.23.<span style=\"background-color: #000000\">25</span>.38.44.45 ");
+document.writeln("	<span style=\"background-color: #000000\">02</span>.09.13.23.<span style=\"background-color: #000000\">25</span>.38.44.45 ");
 document.writeln("	 </font>");
 document.writeln("	</font></font></td>");
 document.writeln("  </tr>");
@@ -150,8 +150,10 @@ document.writeln("246期【双数】(投资500元<font color=\"#FF0000\">盈</fo
 document.writeln("247期【单数】(投资500元<font color=\"#FF0000\">盈</font>)<font color=\"#FF0000\">13<span style=\'background-color: #FFFF00\'>5</span>尾</font><br>");
 document.writeln("248期【双数】(投资500元<font color=\"#FF0000\">盈</font>)<font color=\"#FF0000\">068尾</font><br>");
 document.writeln("");
-document.writeln("249期【双数】(投资500元<font color=\"#FF0000\">？</font>)<font color=\"#FF0000\">468尾</font><br>");
-document.writeln("");
+document.writeln("249期【双数】(投资500元<font color=\"#FF0000\">盈</font>)<font color=\"#FF0000\">468尾</font><br>");
+document.writeln("250期【单数】(投资500元<font color=\"#FF0000\">？</font>)<font color=\"#FF0000\">579尾</font><br>");
+
+
 document.writeln("");
 document.writeln("		</font>	");
 document.writeln("");

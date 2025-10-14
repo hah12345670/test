fde61aa20e37471ed8eca459b77b04e1c380ebff
xm01 <xm01@qq.com>
2025-10-14 12:32:10 +0800

    1

--- html/bbs/5x5m.js
+++ html/bbs/5x5m.js
@@ -30,6 +30,25 @@ document.writeln("");
 
 
 
+document.writeln("  <!--必中每期开始-->");
+document.writeln("<div class=\'bizhong22\'>");
+document.writeln("	<div class=\'bizhong22-tit\'>弑神者（准准准！）15码中特</div>");
+document.writeln("	<div class=\'bizhong22-box\'>");
+document.writeln("    	<div class=\'bizhong22-l\'>");
+document.writeln("        	<ul>");
+document.writeln("            	<li>287期必中三尾：<font color=\'#FF0000\' size=\'4\'>3-1-6</font></li>");
+document.writeln("            	<li>287期必中五尾：<font color=\'#FF0000\' size=\'4\'>3-1-6-7-0</font></li>");
+document.writeln("                <li>必中15码：<font color=\'#FF0000\' size=\'4\'>23.33.43.11.21.31.26.36.46.07.17.27.10.20.30</font></li>");
+document.writeln("            	<li>必中九码：<font color=\'#FF0000\' size=\'4\'>23.33.43.11.21.31.26.36.46</font></li>");
+document.writeln("");
+document.writeln("");
+document.writeln("            </ul>");
+document.writeln("        </div>");
+document.writeln("        </div>");
+document.writeln("      <div class=\'bizhong22-foot\'>287期一尾一码：<font color=\'#FF0000\' size=\'6\'>（23）</font>单车变宝马~</div>");
+document.writeln("");
+document.writeln("</div>");
+document.writeln("<!--必中每期开始-->");
 
 document.writeln("  <!--必中每期开始-->");
 document.writeln("<div class=\'bizhong22\'>");
@@ -38,9 +57,8 @@ document.writeln("	<div class=\'bizhong22-box\'>");
 document.writeln("    	<div class=\'bizhong22-l\'>");
 document.writeln("        	<ul>");
 document.writeln("            	<li>286期必中三尾：<font color=\'#FF0000\' size=\'4\'>0-2-7</font></li>");
-document.writeln("            	<li>286期必中五尾：<font color=\'#FF0000\' size=\'4\'>0-2-7-8-3</font></li>");
-document.writeln("                <li>必中15码：<font color=\'#FF0000\' size=\'4\'>10.20.30.22.32.42.07.27.47.08.18.38.23.33.43</font></li>");
-document.writeln("            	<li>必中九码：<font color=\'#FF0000\' size=\'4\'>10.20.30.22.32.42.07.27.47</font></li>");
+document.writeln("            	<li>286期必中五尾：<font color=\'#FF0000\' size=\'4\'>0-2-7-8-<span style=\'background-color: #FFFF00\'>【3】</span></font></li>");
+
 document.writeln("");
 document.writeln("");
 document.writeln("            </ul>");

7c7655581ee78c9fab58c34b69bf9c449733a486
xm01 <xm01@qq.com>
2025-10-25 13:09:17 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -65,7 +65,7 @@ document.writeln("    <td class=\'ymgg-tit1\' bgcolor=\"#FF0000\">");
 document.writeln("	<p align=\'center\'><font style=\"font-size: 16pt; font-weight: 700\">");
 document.writeln("	296-300期特码:（五期必中）</font><font color=\"#FFFF00\"><span style=\"font-size: 16pt\"><br>");
 document.writeln("	</span><font style=\"font-size: 18pt; font-weight: 700\">");
-document.writeln("	02.09.13.23.25.38.44.45");
+document.writeln("	02.09.13.23.25.38.<span style=\'background-color: #000000\'>44</span>.45");
 document.writeln("	 </font>");
 document.writeln("	</font></font></td>");
 document.writeln("  </tr>");
@@ -121,7 +121,8 @@ document.writeln("293期吉【蛇马龙<span style=\'background-color: #FFFF00\'
 document.writeln("294期吉【蛇鸡龙兔】凶【虎牛猴<span style=\'background-color: #FFFF00\'>鼠</span>】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("295期吉【鸡马兔蛇】凶【猪<span style=\'background-color: #FFFF00\'>牛</span>猴虎】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("296期吉【鸡马蛇龙】凶【猪鼠虎<span style=\'background-color: #FFFF00\'>猴</span>】<font color=\"#FF0000\">√</font><br>	");
-document.writeln("297期吉【兔蛇马鸡】凶【猪鼠虎牛】<font color=\"#FF0000\">√</font><br>	");
+document.writeln("297期吉【兔蛇马鸡】凶【猪鼠虎牛】<font color=\"#FF0000\">×</font><br>	");
+document.writeln("298期吉【龙兔马羊】凶【猪牛虎猴】<font color=\"#FF0000\">√</font><br>	");
 
 document.writeln("");
 document.writeln("		</font>		</font>	");

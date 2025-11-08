047e275886fcdc429e7acd6e2d2ac39792c2bcb1
xm01 <xm01@qq.com>
2025-11-08 17:44:16 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -76,7 +76,8 @@ document.writeln("307期单【猪牛鸡兔】双【虎鼠<span style=\'backgroun
 document.writeln("308期单【羊<span style=\'background-color: #FFFF00\'>兔</span>鸡猪】双【鼠马猴龙】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("309期单【羊猪兔牛】双【马<span style=\'background-color: #FFFF00\'>鼠</span>龙猴】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("310期单【牛羊猪鸡】双【鼠马龙狗】<font color=\"#FF0000\">×</font><br>	");
-document.writeln("311期单【鸡羊兔蛇】双【鼠狗虎马】<font color=\"#FF0000\">√</font><br>	");
+document.writeln("311期单【鸡羊兔<span style=\'background-color: #FFFF00\'>蛇</span>】双【鼠狗虎马】<font color=\"#FF0000\">√</font><br>	");
+document.writeln("312期单【兔鸡牛猪】双【鼠猴虎狗】<font color=\"#FF0000\">√</font><br>	");
 
 
 

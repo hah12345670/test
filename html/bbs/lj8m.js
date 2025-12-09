4b03e6fa77f9dfff319eb1cc08becb5117c58db7
xm01 <xm01@qq.com>
2025-12-09 15:43:04 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -90,7 +90,7 @@ document.writeln("");
 document.writeln("<!--开始-->");
 document.writeln("<table style=border-collapse:collapse border=1 width=100% bgcolor=#ffffff height=\'41\'><tbody><tr>");
 document.writeln("	<td width=100% height=41><p align=center><font face=\'楷体\'><b><font size=\'4\'>");
-document.writeln("	<font color=\'#0000FF\'>343期</font>【<font color=\'#FF0000\'>62827.com看料</font>】开<font color=\'#FF00FF\'>(？00)</font></font></b></font></table>");
+document.writeln("	<font color=\'#0000FF\'>343期</font>【<font color=\'#FF0000\'>独肖:虎羊鼠</font>】开<font color=\'#FF00FF\'>(？00)</font></font></b></font></table>");
 document.writeln("		<!--结束-->");
 document.writeln("");
 document.writeln("");

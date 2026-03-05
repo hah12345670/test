0ce04464d3f457283d6e556768b1cdbf820e0f6d
xm01 <xm01@qq.com>
2026-03-05 16:38:42 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -127,7 +127,7 @@ document.writeln("060期家【羊鸡猪<span style=\'background-color: #FFFF00\'
 document.writeln("061期家【猪狗马鸡】野【蛇猴鼠<span style=\'background-color: #FFFF00\'>虎</span>】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("062期家【马牛猪<span style=\'background-color: #FFFF00\'>羊</span>】野【蛇虎兔龙】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("063期家【羊鸡牛马】野【虎猴龙<span style=\'background-color: #FFFF00\'>兔</span>】<font color=\"#FF0000\">√</font><br>	");
-document.writeln("064期家【62785.com首页来料】<font color=\"#FF0000\">√</font><br>	");
+document.writeln("064期家【牛狗鸡猪】野【虎鼠龙蛇】<font color=\"#FF0000\">√</font><br>	");
 
 document.writeln("");
 document.writeln("");

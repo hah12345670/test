caa812db2f641df5463b82a1ea3d0f38326d923f
xm01 <xm01@qq.com>
2026-06-11 23:36:30 +0800

    1

--- html/bbs/2x8m.js
+++ html/bbs/2x8m.js
@@ -14,13 +14,15 @@ document.writeln("    	<tr>");
 document.writeln("      <td height=25 >");
 document.writeln("		<p align=\"center\"><b>");
 document.writeln("");
-document.writeln("		<font color=\'#FF0000\' face=\'微软雅黑\' style=\"font-size: 16pt\">（162期开？00）</font><font face=\'隶书\' size=\'5\' color=\'#000000\'><br>");
+document.writeln("		<font color=\'#FF0000\' face=\'微软雅黑\' style=\"font-size: 16pt\">（163期开？00）</font><font face=\'隶书\' size=\'5\' color=\'#000000\'><br>");
 document.writeln("		</font>  ");
 document.writeln("		<font face=\'隶书\' size=\'5\'>");
-document.writeln("		马虎+09.21.45.04.16.40.24.48</font></b></td>");
+document.writeln("		猴兔+12.24.36.07.19.31.13.37</font></b></td>");
 document.writeln("    </tr>      ");
 document.writeln("");
 document.writeln("    <!--开始-->   ");
+
+
 
 document.writeln("    <!--开始-->   ");
 document.writeln("    	<tr>");

487144cdf585b434f0006ce72fda05d3212263db
xm01 <xm01@qq.com>
2026-07-02 23:29:15 +0800

    1

--- html/bbs/nypt3.js
+++ html/bbs/nypt3.js
@@ -17,10 +17,16 @@ document.writeln("");
 document.writeln("<table width=\'100%\' id=\'table1\' cellspacing=\'0\' cellpadding=\'0\'>");
 document.writeln("  <tbody>");
 
+document.writeln("<tr>");
+document.writeln("<td align=\'center\' height=40 class=\'stylelxz\'><strong>");
+document.writeln("<font size=\"4\" face=\"微软雅黑\">184期平特3.码<font color=\"#008000\">【虎05.17.41】</font>开:00准</font></strong>");
+document.writeln("</td>");
+document.writeln("</tr>	");
+
 
 document.writeln("<tr>");
 document.writeln("<td align=\'center\' height=40 class=\'stylelxz\'><strong>");
-document.writeln("<font size=\"4\" face=\"微软雅黑\">183期平特3.码<font color=\"#008000\">【狗21.33.45】</font>开:00准</font></strong>");
+document.writeln("<font size=\"4\" face=\"微软雅黑\">183期平特3.码<font color=\"#008000\">【狗21.33.45】</font>开:09准</font></strong>");
 document.writeln("</td>");
 document.writeln("</tr>	");
 

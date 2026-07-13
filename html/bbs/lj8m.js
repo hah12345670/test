72fd8b37decf080ed257c298e0c296e0c3d28682
xm01 <xm01@qq.com>
2026-07-13 22:05:49 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -36,7 +36,7 @@ document.writeln("    <td class=\'ymgg-tit1\' bgcolor=\"#FF0000\">");
 document.writeln("	<p align=\'center\'><font style=\"font-size: 16pt; font-weight: 700\">");
 document.writeln("	191-195期特码:（五期必中）</font><font color=\"#FFFF00\"><span style=\"font-size: 16pt\"><br>");
 document.writeln("	</span><font style=\"font-size: 18pt; font-weight: 700\">");
-document.writeln("	02.<span style=\'background-color: #FFFF00\'>09</span>.13.23.<span style=\'background-color: #000000\'>25</span>.38.44.45");
+document.writeln("	02.<span style=\'background-color: #000000\'>09</span>.13.23.<span style=\'background-color: #000000\'>25</span>.38.44.45");
 document.writeln("	 </font>");
 document.writeln("	</font><span style=\"background-color: #000000\"></font></span></td>");
 document.writeln("  </tr>");
@@ -88,7 +88,7 @@ document.writeln("190期家【马牛猪羊】野【蛇虎<span style=\'backgroun
 document.writeln("191期家【马猪羊牛】野【龙鼠兔<span style=\'background-color: #FFFF00\'>虎</span>】<font color=\"#FF0000\">√</font><br>	");
 document.writeln("192期家【牛狗鸡猪】野【虎鼠龙蛇】<font color=\"#FF0000\">×</font><br>	");
 document.writeln("193期家【猪<span style=\'background-color: #FFFF00\'>狗</span>马鸡】野【蛇猴鼠虎】<font color=\"#FF0000\">√</font><br>	");
-document.writeln("194期家【62827.com首页查看】<font color=\"#FF0000\">√</font><br>	");
+document.writeln("194期家【羊鸡牛马】野【虎猴龙兔】<font color=\"#FF0000\">√</font><br>	");
 
 
 document.writeln("");

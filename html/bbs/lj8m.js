890e1b8dffcd39767c103ce4faa1d98f41bb3588
xm01 <xm01@qq.com>
2026-08-13 23:01:27 +0800

    1

--- html/bbs/lj8m.js
+++ html/bbs/lj8m.js
@@ -32,6 +32,16 @@ document.writeln("	</font><span style=\"background-color: #000000\"></font></spa
 document.writeln("  </tr>");
 document.writeln("");
 document.writeln("");
+document.writeln("      <tr>");
+document.writeln("    <td class=\'ymgg-tit1\' bgcolor=\"#FF0000\">");
+document.writeln("	<p align=\'center\'><font style=\"font-size: 16pt; font-weight: 700\">");
+document.writeln("226-230期特码:（五期必中）</font><font color=\"#FFFF00\"><span style=\"font-size: 16pt\"><br>");
+document.writeln("	</span><font style=\"font-size: 18pt; font-weight: 700\">");
+document.writeln("	02.09.13.23.25.38.44.45");
+document.writeln("	 </font>");
+document.writeln("	</font><span style=\"background-color: #000000\"></font></span></td>");
+document.writeln("  </tr>");
+document.writeln("");
 document.writeln("");
 document.writeln("");
 document.writeln("	<tr>");
@@ -87,7 +97,8 @@ document.writeln("221期：【书画棋琴】琴<span style=\'background-color: 
 document.writeln("222期：【书画棋琴】棋书=开：蛇26错<br><br>");
 document.writeln("223期：【书画棋琴】琴<span style=\'background-color: #FFFF00\'>画</span>=开：猴23准<br><br>");
 document.writeln("224期：【书画棋琴】画<span style=\'background-color: #FFFF00\'>棋</span>=开：狗09准<br><br>");
-document.writeln("225期：【书画棋琴】棋琴=开：？00准<br><br>");
+document.writeln("225期：【书画棋琴】棋琴=开：马01错<br><br>");
+document.writeln("226期：【书画棋琴】62827.com高手榜查看=开：？00准<br><br>");
 
 document.writeln("");
 document.writeln("	");
